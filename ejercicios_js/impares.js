598f2bb1e3cad3013dea0d6b81c823aae007bcb3
franOrellana <orellanafrancisco98@hotmail.com>
2024-10-28 16:51:03 -0300

    Agregada funcionn para mostrar numeros impares

---
+++ ejercicios_js/impares.js
@@ -0,0 +1,13 @@
+// 1. Crear un algoritmo que muestre los números impares entre el 0 y el 100.
+
+
+function mostrarImpares(inicio, fin) {
+  for (let i = inicio; i <= fin; i++) {
+    if (i % 2 !== 0) { 
+      console.log(i); 
+    }
+  }
+}
+
+mostrarImpares(0, 20);
+
