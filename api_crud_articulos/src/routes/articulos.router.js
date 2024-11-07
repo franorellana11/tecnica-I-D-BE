33139ca071d5455573fdadc779d4b9c39f48c494
franOrellana <orellanafrancisco98@hotmail.com>
2024-11-07 17:52:56 -0300

    Inicio de documentacion escrita en router especifico. Get y post

--- api_crud_articulos/src/routes/articulos.router.js
+++ api_crud_articulos/src/routes/articulos.router.js
@@ -11,6 +11,38 @@ const validatorHandler = require('./../middlewares/validator.handler');
 const router = express.Router();
 const service = new ArticulosService();
 
+/**
+ * @swagger
+ * /api/articulos:
+ *   get:
+ *     summary: Obtiene todos los articulos o filtrados por ID, nombre o estado de activación.
+ *     description: Este endpoint GET permite filtrar por coincidencia del nombre, estado de actividad y búsqueda exacta.
+ *     tags:
+ *       - Articulos
+ *     parameters:
+ *       - in: query
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: false
+ *         description: ID del artículo a obtener
+ *       - in: query
+ *         name: nombre
+ *         schema:
+ *           type: string
+ *         description: Nombre del artículo a buscar
+ *       - in: query
+ *         name: estado_activacion
+ *         schema:
+ *           type: boolean
+ *         description: Estado de activación del artículo
+ *     responses:
+ *       200:
+ *         description: Lista de artículos
+ *       404:
+ *         description: Artículo no encontrado
+ */
+
 router.get('/', async (req, res, next) => {
   try {
     const { id, nombre, exact, estado_activacion } = req.query;
@@ -20,11 +52,57 @@ router.get('/', async (req, res, next) => {
       exact,
       estado_activacion,
     });
+    if (articulos.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'No se encontraron artículos con esos parámetros.' });
+    }
     res.json(articulos);
   } catch (error) {
     next(error);
   }
 });
+
+/**
+ * @swagger
+ * /api/articulos/{id}:
+ *   get:
+ *     summary: Obtiene un artículo específico
+ *     description: Retorna un artículo basado en el ID proporcionado.
+ *     tags:
+ *       - Articulos
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID del artículo
+ *     responses:
+ *       200:
+ *         description: Artículo encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   example: "123"
+ *                 nombre:
+ *                   type: string
+ *                   example: "Nombre del artículo"
+ *                 fecha_modificacion:
+ *                   type: date
+ *                   example: "2024-10-29T15:45:45.075Z"
+ *                 estado_activacion:
+ *                   type: boolean
+ *                   example: true
+ *       404:
+ *         description: Artículo no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
 
 router.get(
   '/:id',
@@ -40,6 +118,58 @@ router.get(
   }
 );
 
+/**
+ * @swagger
+ * /api/articulos:
+ *   post:
+ *     summary: Crea un nuevo artículo
+ *     description: Este endpoint permite crear un nuevo artículo.
+ *     tags:
+ *       - Articulos
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *                 example: "Nombre del artículo"
+ *               descripcion:
+ *                 type: string
+ *                 example: "Descripción detallada del artículo"
+ *               marca:
+ *                 type: string
+ *                 example: "Marca del artículo"
+ *               estado_activacion:
+ *                 type: boolean
+ *                 example: true
+ *     responses:
+ *       201:
+ *         description: Artículo creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   example: "123"
+ *                 nombre:
+ *                   type: string
+ *                   example: "Nombre del artículo"
+ *                 fecha_modificacion:
+ *                   type: date
+ *                   example: "2024-10-29T15:45:45.075Z"
+ *                 estado_activacion:
+ *                   type: boolean
+ *                   example: true
+ *       400:
+ *         description: Error de validación en la solicitud
+ *       500:
+ *         description: Error en el servidor
+ */
 router.post(
   '/',
   validatorHandler(createArticuloSchema, 'body'),
